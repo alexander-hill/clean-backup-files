6f619167f288cf5dab2c71dbc41f92461371f787
Alex Hill <alexander.d.hill.89@gmail.com>
2016-02-06 21:54:40 -0800

    Add file local variable line
    It's been annoying to have to manually set Scheme mode.

--- main.scm
+++ main.scm
@@ -1,4 +1,5 @@
 #!/bin/sh
+# -*- mode: scheme; -*-
 exec "${GUILE-guile}" -s "$0" "$@"
 !#
 
@@ -9,16 +10,14 @@ exec "${GUILE-guile}" -s "$0" "$@"
     (let filter-files ((dir-entry (readdir dir-stream)))
       (if (not (eof-object? dir-entry))
 	  (let ((entry-full-path (string-append dir "/" dir-entry)))
-	    (begin
-	      (if (and (file-is-directory? entry-full-path)
-		       (not (or (equal? "." dir-entry)
-				(equal? ".." dir-entry)
-				(equal? ".git" dir-entry))))
-		  (filter-dir entry-full-path)
-		  (begin
-		    (if (regexp-exec emacs-backup-pattern dir-entry)
-			(delete-file entry-full-path))
-		    (filter-files (readdir dir-stream))))))))
+	    (if (and (file-is-directory? entry-full-path)
+		     (not (or (equal? "." dir-entry)
+			      (equal? ".." dir-entry)
+			      (equal? ".git" dir-entry))))
+		(filter-dir entry-full-path)
+		(if (regexp-exec emacs-backup-pattern dir-entry)
+		    (delete-file entry-full-path)))
+	    (filter-files (readdir dir-stream)))))
     (closedir dir-stream)))
 
 (filter-dir (getcwd))
